01ed62d6fadd670c651dcc86e8bb0f2aaed7c52a
Tristan Hinkel <tristan_hinkel@web.de>
2024-08-03 13:28:00 +0200

    Frontend V.2

--- package_folder/frontend_file.py
+++ package_folder/frontend_file.py
@@ -5,9 +5,90 @@ from decimal import Decimal
 import folium
 from streamlit_folium import st_folium
 import requests
+import os
+from st_aggrid import AgGrid, GridOptionsBuilder
+
+
+# Get the directory of the current script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Define the relative path to the header image
+relative_path = os.path.join("frontend_data", "flatquest_header.jpg")
+
+# Construct the absolute path
+image_path = os.path.join(current_dir, relative_path)
+
+
+# Display the header image
+st.image(image_path, use_column_width=True)
 
 # Title and description
-st.title("📍 Location Display on Map")
+st.title("Find an appartment")
+
+# Sample data - replace with your database query or CSV file
+cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix",
+          "Philadelphia", "San Antonio", "San Diego", "Dallas", "San Jose"]
+
+# Convert list to DataFrame
+cities_df = pd.DataFrame(cities, columns=["City"])
+
+
+# Create an input field with autocomplete
+def city_autocomplete():
+    input_city = st.text_input("Enter city name", key="city_input")
+    if input_city:
+        # Filter cities based on input
+        filtered_cities = cities_df[cities_df['City'].str.contains(input_city, case=False, na=False)]
+
+        if not filtered_cities.empty:
+            selected_city = st.radio(
+                "Suggestions",
+                filtered_cities['City'].tolist(),
+                key="suggestions_radio"
+            )
+            return selected_city
+
+    return input_city
+
+selected_city = city_autocomplete()
+
+if selected_city:
+    st.write(f"You selected: {selected_city}")
+
+total_rent = st.text_input("Enter desired total rent in €", key="total rent")
+
+living_space = st.text_input("Enter desired living space in m²", key="living space")
+
+no_rooms = st.text_input("Enter desired number of rooms", key="no rooms")
+
+balcony = st.radio(f"Do you want a balcony?", ('yes', 'no'), index=1)
+
+# Data conversion and validation
+try:
+    total_rent = float(total_rent) if total_rent else None
+except ValueError:
+    st.error("Please enter a valid number for total rent.")
+
+try:
+    living_space = float(living_space) if living_space else None
+except ValueError:
+    st.error("Please enter a valid number for living space.")
+
+try:
+    no_rooms = int(no_rooms) if no_rooms else None
+except ValueError:
+    st.error("Please enter a valid integer for number of rooms.")
+
+balcony = "True" if balcony == 'yes' else "False"
+
+# Display the inputs and their types
+#st.write(f"Total Rent: {total_rent} (type: {type(total_rent)})")
+#st.write(f"Living Space: {living_space} (type: {type(living_space)})")
+#st.write(f"Number of Rooms: {no_rooms} (type: {type(no_rooms)})")
+#st.write(f"Balcony: {balcony} (type: {type(balcony)})")
+
+if selected_city:
+    st.write(f"You selected: {selected_city}")
 
 # Define unique categories
 categories = [
@@ -24,27 +105,30 @@ categories = [
 selected_categories = []
 category_density = {}
 
-st.write("### Select Categories and Density")
 
-st.write("Please select three categories of places of interest and choose a level of density for the search")
+st.write("Please select your places of interest")
 
 # Track the number of selected checkboxes
 selected_count = 0
+
+#Slider options
+options = ["Some", "Average", "Many"]
 
 for i in range(0, len(categories), 3):
     cols = st.columns(3)
     for j, category in enumerate(categories[i:i + 3]):
         with cols[j]:
             if selected_count < 3:
-                selected = st.checkbox(category, value=False)
+                selected = st.checkbox(category, key=f"checkbox_{category}", value=False)
                 if selected:
                     selected_categories.append(category)
                     selected_count += 1
-                    density = st.radio(f"Density for {category}", ('high', 'medium', 'low'), index=1)
+                    #density = st.radio(f"Density for {category}", ('high', 'medium', 'low'), index=1)
+                    # Create a slider with custom options
+                    density = st.select_slider("Select desired number:", options=options, key=f"slider_{category}")
                     category_density[category] = density
             else:
-                st.checkbox(category, value=False, disabled=True)
-
+                st.checkbox(category, value=False, key=f"checkbox_{category}", disabled=True)
 
 #API Request
 
@@ -112,8 +196,13 @@ if show_results:
         # Define colors for each category
         category_colors = {
             'gym': 'red',
-            'restaurant': 'green',
-            'school': 'blue'
+            'restaurant': 'purple',
+            'school': 'blue',
+            'bar': 'yellow',
+            'park': 'green',
+            'cafe': 'brown',
+            'hospital': 'white',
+            'transit': 'grey'
         }
 
         # Add points of interest to the map
